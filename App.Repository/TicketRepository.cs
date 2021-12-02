9f2e48076d8fd511c5c085ad68739dd0a1805f5b
Demonick <masterprogger87@gmail.com>
2021-12-02 21:08:13 +0200

    All Tests done

---
+++ App.Repository/TicketRepository.cs
@@ -0,0 +1,60 @@
+﻿using App.Repository.ApiClient;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repository
+{
+    // 87.0
+    public class TicketRepository
+    {
+        // 87.1
+        private readonly IWebApiExecuter _webApiExecuter;
+
+        public TicketRepository(IWebApiExecuter webApiExecuter)
+        {
+            _webApiExecuter = webApiExecuter;
+        }
+
+        // 87.2 Создаём метод получения всех проектов
+        public async Task<IEnumerable<Ticket>> GetAsync(string filter = null)
+        {
+            // Устанавливаем версию API в строку браузера
+            string uri = "api/tickets?api-version=2.0";
+            if (!string.IsNullOrWhiteSpace(filter))
+                uri += $"&titleorderdescription={filter.Trim()}";
+
+            return await _webApiExecuter.InvokeGet<IEnumerable<Ticket>>(uri);
+        }
+
+        // 87.3 Создаём метод получения проекта по ID
+        public async Task<Ticket> GetByIdAsync(int id)
+        {
+            return await _webApiExecuter.InvokeGet<Ticket>($"api/tickets/{id}?api-version=2.0");
+        }
+
+        // 87.4 Создаём метод создания проекта
+        public async Task<int> CreateAsync(Ticket ticket)
+        {
+            ticket = await _webApiExecuter.InvokePost("api/tickets?api-version=2.0", ticket);
+            return ticket.Id;
+        }
+
+        // 87.5 Создаём метод обновления проекта
+        public async Task UpdateAsync(Ticket ticket)
+        {
+            await _webApiExecuter.InvokePut($"api/tickets/{ticket.Id}?api-version=2.0", ticket);
+        }
+
+        // 87.6 Создаём метод обновления проекта
+        public async Task DeleteAsync(int id)
+        {
+            await _webApiExecuter.InvokeDelete($"api/tickets/{id}?api-version=2.0");
+        }
+    }
+}
+
+// -->87.7 Далее идём в консольный класс Program и пишем тесты для нового контроллера
+
--- App.Testing.Console/Program.cs
+++ App.Testing.Console/Program.cs
@@ -41,8 +41,81 @@ await DeleteProject(proj);
 await GetProjects();
 
 
+// 87.8 ************************************************
+WriteLine("-----------------------------------");
+WriteLine("Reading all tickets");
+
+await GetTickets();
+
+WriteLine("Tickets is readed! Next step start");
+WriteLine("------------------------------------");
+
+WriteLine("------------------------------------");
+WriteLine("Create a new ticket");
+
+var tik = await CreateProject();
+await GetTickets();
+
+WriteLine("------------------------------------");
+WriteLine("Update a ticket");
+
+await UpdateTickets(tik);
+await GetTickets();
+
+WriteLine("------------------------------------");
+WriteLine("Delete a ticket");
+
+await DeleteTickets(tik);
+await GetTickets();
+// *****************************************************
+
 // 85.1 Создаём метод для тестирования получения всех проектов
 // Предварительно добавив ссылки на проекты Core и Repository
+async Task GetTickets()
+{
+    // 85.3 Создаём экземпляр репозитория
+    ProjectRepository repository = new(webApiExecuter);
+
+    // 85.4 Получаем все проекты
+    var projects = await repository.GetAsync();
+
+    // 85.5 Выводим на консоль все проекты
+    foreach (var item in projects)
+    {
+        WriteLine($"Ticket : {item.Name}");
+    }
+    // После этого устанавливаем в Solution Properties проекты для запуска
+}
+
+async Task<Project> GetTicket(int id)
+{
+    ProjectRepository repository = new(webApiExecuter);
+    return await repository.GetByIdAsync(id);
+}
+
+async Task<int> CreateTickets()
+{
+    var project = new Project { Name = "Another Ticket 3" };  // <-- Для теста ошибок, делаем null
+    ProjectRepository repository = new(webApiExecuter);
+    return await repository.CreateAsync(project);
+}
+
+async Task UpdateTickets(int id)
+{
+    ProjectRepository repository = new(webApiExecuter);
+    var project = await repository.GetByIdAsync(id);
+    project.Name = " UPDATED"; // <-- Для теста ошибок, делаем null
+    await repository.UpdateAsync(project);
+}
+
+async Task DeleteTickets(int id)
+{
+    ProjectRepository repository = new(webApiExecuter);
+    await repository.DeleteAsync(id);
+}
+
+
+
 async Task GetProjects()
 {
     // 85.3 Создаём экземпляр репозитория
@@ -75,7 +148,7 @@ async Task GetProjectTickets(int id)
 
     foreach (var ticket in tickets)
     {
-        WriteLine($"Ticket: {ticket.Title}");
+        WriteLine($"Project: {ticket.Title}");
     }
 }
 
